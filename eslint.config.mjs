6f79328055c9e5def7601f945ccf605e08283039
Suchetamon27 <suchetamonbol@gmail.com>
2025-10-20 14:17:44 +0530

    Add simple-import-sort plugin for automatic import organization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort"; // New Plugin
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
+const eslintConfig = [...compat.extends("next/core-web-vitals"),
+                      {
+    plugins: {
+      "simple-import-sort": simpleImportSort, // Register plugin
+    },
+    rules: {
+      //New Feature: Automatically sort imports and exports
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+];
 export default eslintConfig;
