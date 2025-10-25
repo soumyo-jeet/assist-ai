c7bbffab45832b03e0c356ab85bd3f9459dc4d8e
Suchetamon27 <suchetamonbol@gmail.com>
2025-10-25 14:00:09 +0530

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
+import autoprefixer from "autoprefixer",
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    "@tailwindcss/postcss",
+  autoprefixer,
+],
 };
 
 export default config;
